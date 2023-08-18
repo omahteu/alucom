ed94295b7f7ee081e0b343a45f4c99bf0eda3d48
Matheus <matheus.mmn@hotmail.com>
2023-08-18 08:42:08 -0300

    mensagem implementada

--- js/modais/cadastros/mensagem.js
+++ js/modais/cadastros/mensagem.js
@@ -1,3 +1,16 @@
 $(document).on("click", "#abaCadastroMensagem", function() {
-    alert('Construindo')
+    let area = $("#main-content")
+    area.html(
+        `
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="inputPassword4">Mensagem</label>
+                    <textarea class="form-control" name="" id="" cols="30" rows="50"></textarea>
+                </div>
+            </div>
+            <button type="button" class="btn btn-primary">Salvar</button>
+        </form>
+        `
+    )
 })

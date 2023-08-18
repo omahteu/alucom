75513845e5e71b87203e713a146781d0e10e4f52
Matheus <matheus.mmn@hotmail.com>
2023-08-18 11:01:56 -0300

    parte de movimentações implementada

---
+++ js/modais/movimentacoes/substituicao.js
@@ -0,0 +1,75 @@
+$(document).on("click", "#abaMovimentacaoSubstituicao", function() {
+    let area = $("#main-content")
+    area.html(
+        `
+        <form>
+
+            <div class="form-row">
+                <div class="form-group col-md-10">
+                    <label for="inputEmail4">Número ou Nome do Cliente</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Número ou Nome do Cliente">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputEmail4">.</label>
+                    <button type="button" class="form-control">Selecionar</button>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-10">
+                    <label for="inputEmail4">Número ou Nome do Equipamento</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Número ou Nome do Equipamento">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputEmail4">.</label>
+                    <button type="button" class="form-control">Selecionar</button>
+                </div>
+            </div>
+
+
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="inputEmail4">Número do Documento</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Número do Documento">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputEmail4">Tipo de Movimento</label>
+                    <input type="texet" class="form-control" id="inputEmail4" placeholder="Tio de Movimento">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputEmail4">Cliente</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Cliente">
+                </div>
+            </div>
+
+            <br>
+
+            <h2>Novo Equipamento</h2>
+
+            <div class="form-row">
+                <div class="form-group col-md-8">
+                    <label for="inputEmail4">Número ou Nome do Equipamento</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Número ou Nome do Equipamento">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputEmail4">Tipo</label>
+                    <select class="form-control">
+                        <option hidden>Tipo</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-11">
+
+                </div>
+                <div class="form-group col-md-1">
+                    <button class="btn btn-success">Adicionar</button>
+                </div>
+            </div>
+
+            <button type="button" class="btn btn-primary">Salvar</button>
+        </form>
+        `
+    )
+})

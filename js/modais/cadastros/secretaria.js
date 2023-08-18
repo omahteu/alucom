bdd873f0b734f42c656db7f6beffc1234ba39f2c
Matheus <matheus.mmn@hotmail.com>
2023-08-18 08:51:19 -0300

    secretarias implementado

--- js/modais/cadastros/secretaria.js
+++ js/modais/cadastros/secretaria.js
@@ -1,3 +1,22 @@
 $(document).on("click", "#abaCadastroSecretaria", function() {
-    alert('Construindo')
+    let area = $("#main-content")
+    area.html(
+        `
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-8">
+                    <label for="inputPassword4">Nome</label>
+                    <input type="text" class="form-control" placeholder="Nome">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputPassword4">Prefeitura</label>
+                    <select class="form-control">
+                        <option hidden>Prefeitura</option>
+                    </select>
+                </div>
+            </div>
+            <button type="button" class="btn btn-primary">Salvar</button>
+        </form>
+        `
+    )
 })

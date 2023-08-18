d60cd45ecaa3501cefcd0dcacbc872e25341aa48
Matheus <matheus.mmn@hotmail.com>
2023-08-18 08:47:41 -0300

    prefeitura implementado

--- js/modais/cadastros/prefeitura.js
+++ js/modais/cadastros/prefeitura.js
@@ -1,3 +1,16 @@
 $(document).on("click", "#abaCadastroPrefeitura", function() {
-    alert('Construindo')
+    let area = $("#main-content")
+    area.html(
+        `
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="inputPassword4">Nome</label>
+                    <input type="text" class="form-control" placeholder="Nome">
+                </div>
+            </div>
+            <button type="button" class="btn btn-primary">Salvar</button>
+        </form>
+        `
+    )
 })
